1450dbd73e28c77661f9896bc141cb796c3e8cbd
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-02-27 20:19:47 -0500

    Add email validation

--- scripts.js
+++ scripts.js
@@ -3,11 +3,30 @@ const country = document.getElementById('country')
 const zipCode = document.getElementById('zipcode')
 const password = document.getElementById("password")
 const submitButton = document.querySelector("#submit")
+const form = document.getElementById('form')
 
-const inputDiv = document.querySelectorAll('.input-group .message')
+const errorMessage = document.querySelector('.message')
 
-inputDiv.forEach(input => {
-    input.addEventListener('click', () => {
-        console.log(input.textContent)
-    })
-});
+form.addEventListener('submit', (e) => {
+    let messages = []
+
+    validateEmail(email.value, messages)
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMessage.innerText = messages.join(', ')
+    }
+})
+
+function validateEmail(email, messages) {
+    let mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (email.match(mailFormat)) {
+        return true;
+    }
+    else {
+        messages.push('Please enter a valid email address')
+        return false;
+    }
+}
+
+
